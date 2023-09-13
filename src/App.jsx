e21778460bc824d096d4b09fa22f50c629a2d8bb
ridho andhika <ridhoandhika74@gmail.com>
2023-09-13 12:32:56 +0700

    setup project react vite js

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      {/* <h1>home page</h1> */}
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+        type="submit"
+      >
+        Buy now
+      </button>
+    </div>
+  );
+}
+
+export default App;
