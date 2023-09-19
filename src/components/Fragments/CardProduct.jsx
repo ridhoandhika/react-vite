0199417c4b243f59af9b6b38bc012b8d8f2c322b
ridho andhika <ridhoandhika74@gmail.com>
2023-09-19 16:34:41 +0700

    penggunaan state

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -2,7 +2,7 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow mx-2 flex flex-col jutitfy-between">
+    <div className="w-full max-w-xs bg-gray-800 border border-gray-200 rounded-lg shadow mx-3 my-2 flex flex-col jutitfy-between">
       {children}
     </div>
   );
@@ -11,16 +11,12 @@ const Header = (props) => {
   const { image } = props;
   return (
     <a href="#!">
-      <img
-        src={image}
-        alt="product"
-        className="p-8 rounded-t-lg"
-      />
+      <img src={image} alt="product" className="p-8 rounded-t-lg" />
     </a>
   );
 };
 
-// 
+//
 const Body = (props) => {
   const { children, name } = props;
   return (
@@ -36,11 +32,19 @@ const Body = (props) => {
 };
 
 const Footer = (props) => {
-    const { price } = props
+  const { price, handleAddToCart, id } = props;
   return (
     <div className="flex items-center justify-between px-5 pb-5">
-      <span className="text-3xl font-bold text-white">Rp. {price}</span>
-      <Button classname="bg-blue-600">Add to Cart</Button>
+      <span className="text-xl font-bold text-white">
+        {price.toLocaleString("id-ID", {
+          style: "currency",
+          currency: "IDR",
+          minimumFractionDigits: 0,
+        })}
+      </span>
+      <Button classname="bg-blue-600 text-xs" onClick={() => handleAddToCart(id)}>
+        Add to Cart
+      </Button>
     </div>
   );
 };
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
+import { useState } from "react";
 
 const products = [
   {
@@ -25,13 +26,34 @@ const products = [
   },
 ];
 
+//simpan kedalam localstorage
 const email = localStorage.getItem("email");
 
 const ProductPage = () => {
+  const [cart, setCart] = useState([]);
   const handleLogout = () => {
     localStorage.removeItem("email");
     localStorage.removeItem("password");
     window.location.href = "/login";
+  };
+
+  const handleAddToCart = (id) => {
+    //cari id yang sudah ada di dalam cart, jika ada ubah qty nya
+    if (cart.find((item) => item.id === id)) {
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, qty: item.qty + 1 } : item
+        )
+      );
+    } else {
+      setCart([
+        ...cart,
+        {
+          id,
+          qty: 1,
+        },
+      ]);
+    }
   };
   return (
     <>
@@ -42,15 +64,92 @@ const ProductPage = () => {
         </Button>
       </div>
       <div className="flex justify-center py-5">
-        {products.map((product) => (
-          <CardProduct key={product.id}>
-            <CardProduct.Header image={product.image} />
-            <CardProduct.Body name={product.name}>
-              {product.description}
-            </CardProduct.Body>
-            <CardProduct.Footer price={product.price} />
-          </CardProduct>
-        ))}
+        <div className="w-4/6 flex flex-wrap">
+          {products.map((product) => (
+            <CardProduct key={product.id}>
+              <CardProduct.Header image={product.image} />
+              <CardProduct.Body name={product.name}>
+                {product.description}
+              </CardProduct.Body>
+              <CardProduct.Footer
+                id={product.id}
+                price={product.price}
+                handleAddToCart={handleAddToCart}
+              />
+            </CardProduct>
+          ))}
+        </div>
+        <div className="w-2/6 px-2">
+          {cart.length > 0 && (
+            <>
+              <h1 className="text-3xl font-bold text-blue-600 mb-3">Cart</h1>
+              <table className="min-w-full border text-center text-sm font-light dark:border-neutral-500">
+                <thead className="border-b font-medium dark:border-neutral-500">
+                  <tr className="bg-gray-200">
+                    <th
+                      scope="col"
+                      className="border-r px-6 py-4 dark:border-neutral-500"
+                    >
+                      Product
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-r px-6 py-4 dark:border-neutral-500"
+                    >
+                      Price
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-r px-6 py-4 dark:border-neutral-500"
+                    >
+                      Qty
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-r px-6 py-4 dark:border-neutral-500"
+                    >
+                      Total
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cart.map((item) => {
+                    const product = products.find(
+                      (product) => product.id === item.id
+                    );
+                    return (
+                      <tr
+                        className="border-b dark:border-neutral-500"
+                        key={item.id}
+                      >
+                        <td className="whitespace-nowrap border-r px-6 py-4 font-medium dark:border-neutral-500 text-start">
+                          {product.name}
+                        </td>
+                        <td className="whitespace-nowrap border-r px-6 py-4 font-medium dark:border-neutral-500">
+                          {product.price.toLocaleString("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                            minimumFractionDigits: 0,
+                          })}
+                        </td>
+                        <td className="whitespace-nowrap border-r px-6 py-4 font-medium dark:border-neutral-500">
+                          {item.qty}
+                        </td>
+                        <td className="whitespace-nowrap border-r px-6 py-4 font-medium dark:border-neutral-500">
+                          {(item.qty * product.price).toLocaleString("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                            minimumFractionDigits: 0,
+                          })}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
