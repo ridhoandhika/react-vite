efbda59ff4db6b27993a0c8748254d5352662efd
ridho andhika <ridhoandhika74@gmail.com>
2023-09-19 13:56:54 +0700

    rendering list with data

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,17 +1,40 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "sneaker",
+    price: 1000000,
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore voluptatibus ab fugiat quae vero rem alias a incidunt, tempora natus in perferendis! Quam, perspiciatis? Ipsum nulla at porro dolor! In!,`,
+  },
+  {
+    id: 2,
+    name: "Sneaker Nike",
+    price: 2000000,
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+  },
+  {
+    id: 2,
+    name: "Sneaker New Balance",
+    price: 2500000,
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+  },
+];
 const ProductPage = () => {
   return (
-    <div className="flex justify-center">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg"/>
-        <CardProduct.Body title="Nike Jordan">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-          voluptatibus ab fugiat quae vero rem alias a incidunt, tempora natus
-          in perferendis! Quam, perspiciatis? Ipsum nulla at porro dolor! In!
-        </CardProduct.Body>
-        <CardProduct.Footer price="1.000.000"/>
-      </CardProduct>
+    <div className="flex justify-center py-5">
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
